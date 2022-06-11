863918ca9af0f0fd387a417fe8d534423401b863
Guilherme Brito <guilhermebritoalmeidacosta@gmail.com>
2022-06-11 14:37:41 -0300

    66

--- index.js
+++ index.js
@@ -1,71 +1,17 @@
-/* #region Variaveis  */
-const Teclas = [
-    "49",
-    "50",
-    "51",
-    "52",
-    "53",
-    "54",
-    "55",
-    "56",
-    "57",
-    "48",
-    "81",
-    "87",
-    "69",
-    "82",
-    "84",
-    "89",
-    "85",
-    "73",
-    "79",
-    "80",
-    "220",
-    "65",
-    "83",
-    "68",
-    "70",
-    "71",
-    "72",
-    "74",
-    "75",
-    "76",
-    "20",
-    "13",
-    "16",
-    "90",
-    "88",
-    "67",
-    "86",
-    "66",
-    "78",
-    "190",
-    "191",
-    "32",
-    "17"
-]
-const PontuacaoDiv = document.getElementById("pontuacao")
+let teclaRandom
 
-let TeclaAleatoria
-let t = 2000
-
-/* #endregion */
-
-document.addEventListener("keydown", teclaPressionada, false)
-document.addEventListener("keyup", teclaDespressionada, false)
-
-function teclaPressionada(e) {
+const teclaPressionada = (e) => {
     var keyCode = e.keyCode
     var tecla = document.getElementById(keyCode)
 
     if (tecla == undefined) return
 
-    keyCode == TeclaAleatoria ? teclaCerta() : teclaErrada()
+    if (keyCode == teclaRandom) teclaCerta()
 
     tecla.classList.add("pressionado")
 }
 
-function teclaDespressionada(e) {
+const teclaDespressionada = (e) => {
     var keyCode = e.keyCode
     var tecla = document.getElementById(keyCode)
 
@@ -74,20 +20,21 @@ function teclaDespressionada(e) {
     tecla.classList.remove("pressionado")
 }
 
-function teclaAleatoria() {
-    if (TeclaAleatoria != null) document.getElementById(TeclaAleatoria).classList.remove("random")
+document.addEventListener("keydown", teclaPressionada, false)
+document.addEventListener("keyup", teclaDespressionada, false)
 
-    var x = Teclas[Math.floor(Math.random() * Teclas.length)]
 
-    TeclaAleatoria = x
+const teclaCerta = () => {
+    var tecla = document.getElementById(teclaRandom)
 
-    document.getElementById(x).classList.add("random")
-}
+    tecla.classList.remove("random")
 
-function teclaCerta() {
     teclaAleatoria()
 }
 
-function teclaErrada() {
-    teclaAleatoria()
+const teclaAleatoria = () => {
+    teclaRandom = Teclas[Math.floor(Math.random() * Teclas.length)]
+    document.getElementById(teclaRandom).classList.add("random")
 }
+
+teclaAleatoria()
